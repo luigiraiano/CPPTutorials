7890ce5e220ffdc4ef7bdc5c844d1bba390340db
Luigi Raiano <raiano.luigi@gmail.com>
2023-04-27 11:22:57 +0200

    added new thread; added mutex

--- src/nodes/concurrency_main.cpp
+++ src/nodes/concurrency_main.cpp
@@ -8,17 +8,32 @@ static std::mutex lock;
 
 void task2()
 {
-
-    std::cout << "Task 2" << std::endl;
-    uint64_t count = 0;
-    while (count < 20)
+    if( lock.try_lock() )
     {
-        std::cout << "[Task 2] Count: " << count << std::endl;
-        count ++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000) );
+        std::cout << "Task 2" << std::endl;
+        uint64_t count = 0;
+        while (count < 20)
+        {
+            std::cout << "[Task 2] Count: " << count << std::endl;
+            count ++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500) );
+        }
+
+        std::cout << "Task 2 has finished its job" << std::endl;
+        lock.unlock();
     }
 
-    std::cout << "Task 2 has finished its job" << std::endl;
+}
+
+void task3(const std::string& msg)
+{
+    if( lock.try_lock() )
+    {
+        std::cout << "Task 3" << std::endl;
+        std::cout << msg << std::endl;
+        lock.lock();
+    }
+
 }
 
 int main(int argc, char** argv)
@@ -28,6 +43,7 @@ int main(int argc, char** argv)
     std::cout << "Task 1" << std::endl;
 
     std::thread t2(&task2);
+    std::thread t3(&task3, "Thread 3");
 
     uint64_t count = 0;
     while (count < 20)
@@ -43,6 +59,13 @@ int main(int argc, char** argv)
     if(t2.joinable())
     {
         t2.join();
+        std::cout << "Thread " << t2.get_id() << " has finished." << std::endl;
+    }
+
+    if(t3.joinable() )
+    {
+        t3.join();
+        std::cout << "Thread " << t3.get_id() << " has finished." << std::endl;
     }
 
     return 0;
